2a295a93359a2aa256e347f81e70c6bcc335af40
陈逸民 <chenyiminchn@qq.com>
2017-05-13 18:06:47 +0800

    页面变大，增加列表分页

--- caches/caches_commons/caches_data/link.cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  1 => 
-  array (
-    'is_post' => '0',
-    'enablecheckcode' => '0',
-  ),
-);
-?>
--- caches/caches_template/default/content/list_picture.php
+++ caches/caches_template/default/content/list_picture.php
@@ -1,6 +1,7 @@
 <?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><?php include template("content","header"); ?>
 <!--main-->
 <div id="content">
+
             <div class="list_top">
                 <img src="<?php echo IMG_PATH;?>home.png" alt="" />
                 <span><a href="<?php echo APP_PATH;?>">首页 &gt;</a><a href=""><?php echo catpos($catid);?>列表</a></span>
@@ -11,7 +12,7 @@
                         <p><?php echo $CATEGORYS[$catid]['catname'];?></p>
                     </div>
                     <div class="shuang">
-                    <?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=83e86dbe8e634be99f241427bdc4f45f&action=lists&catid=%24catid&num=20&order=id+DESC&page=%24page\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$pagesize = 20;$page = intval($page) ? intval($page) : 1;if($page<=0){$page=1;}$offset = ($page - 1) * $pagesize;$content_total = $content_tag->count(array('catid'=>$catid,'order'=>'id DESC','limit'=>$offset.",".$pagesize,'action'=>'lists',));$pages = pages($content_total, $page, $pagesize, $urlrule);$data = $content_tag->lists(array('catid'=>$catid,'order'=>'id DESC','limit'=>$offset.",".$pagesize,'action'=>'lists',));}?>
+                    <?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=dc37b095520fb38bcb109a973baebf72&action=lists&catid=%24catid&num=20&order=listorder+DESC&page=%24page\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$pagesize = 20;$page = intval($page) ? intval($page) : 1;if($page<=0){$page=1;}$offset = ($page - 1) * $pagesize;$content_total = $content_tag->count(array('catid'=>$catid,'order'=>'listorder DESC','limit'=>$offset.",".$pagesize,'action'=>'lists',));$pages = pages($content_total, $page, $pagesize, $urlrule);$data = $content_tag->lists(array('catid'=>$catid,'order'=>'listorder DESC','limit'=>$offset.",".$pagesize,'action'=>'lists',));}?>
                         <ul>
                         <?php $n=1;if(is_array($data)) foreach($data AS $r) { ?>
                             <li>
