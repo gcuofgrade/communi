00a55cc176352d72771fd2bec18e06aecf0f5642
陈逸民 <chenyiminchn@qq.com>
2017-07-06 16:07:42 +0800

    图标修改

--- caches/caches_template/default/content/footer.php
+++ caches/caches_template/default/content/footer.php
@@ -1,7 +1,7 @@
 <?php defined('IN_PHPCMS') or exit('No permission resources.'); ?>	<div id="foot">
 	           <div class="last">
 	                <p>COPYRIGHT © 2016 华南理工大学广州学院国际教育与交流中心</p>
-	                <p>ALL RIGHTS RESERVED. DESIGNED BY 图书馆数字建设部. 办公地址: 广州市花都区学府路1号华工广州学院行政楼412   技术报障:</p>
+	                <p>ALL RIGHTS RESERVED. DESIGNED BY 图书馆数字建设部. 办公地址: 广州市花都区学府路1号行政楼412   技术报障:</p>
 	           </div>
 	        </div>
 	    </div>
